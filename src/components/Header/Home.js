1f7a8c6eca601f54aeec3064d6888a5c89e2a27c
kimdolion <kimowilkes@gmail.com>
2019-11-11 11:26:52 -0500

    Add margin to home description

--- src/components/Header/Home.js
+++ src/components/Header/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 
+const styles = {
+  main: {
+    margin: '15px'
+  }
+}
+
 const authenticatedOptions = (
   <React.Fragment>
     <p>Create a new list, update an old list, or look at what other&aposs hope to accomplish.
@@ -18,14 +24,14 @@ const unauthenticatedOptions = (
 )
 
 const Home = ({ user }) => (
-  <React.Fragment>
+  <div style={styles.main}>
     <h1>Welcome to My First Hundred!</h1>
     <p>What are the first 100 activities that you want to accomplish?
     </p>
     <p>You can also see what other people are working towards.
     </p>
     { user ? authenticatedOptions : unauthenticatedOptions }
-  </React.Fragment>
+  </div>
 )
 
 export default Home
